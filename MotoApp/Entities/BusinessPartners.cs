d2e356fe2392e40697af422d9026b3139345499a
Piotr Rogalewski <piotr.rogalewski@gmail.com>
2023-08-08 20:37:04 +0200

    Dodaj pliki projektów.

---
+++ MotoApp/Entities/BusinessPartners.cs
@@ -0,0 +1,10 @@
+﻿namespace MotoApp.Entities
+{
+    public class BusinessPartners : EntityBase
+
+    {
+        public string? Name { get; set; }
+
+        public override string ToString() => $"Id: {Id}, First name: {Name}";
+    }
+}
---
+++ MotoApp/Entities/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace MotoApp.Entities
+{
+    public interface IEntity
+    {
+        public int Id { get; set; }
+    }
+}
